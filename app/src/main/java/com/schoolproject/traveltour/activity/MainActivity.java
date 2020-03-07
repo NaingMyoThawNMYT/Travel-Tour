787989feb7dc5aae04efd2885d0f1fdcd410dce6
NaingMyoThawNMYT <naingmyothaw.nmyt@gmial>
2020-03-07 15:29:11 +0630

    In main page, add adapter click listener.

--- app/src/main/java/com/schoolproject/traveltour/activity/MainActivity.java
+++ app/src/main/java/com/schoolproject/traveltour/activity/MainActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.schoolproject.traveltour.R;
 import com.schoolproject.traveltour.adapter.MenuAdapter;
+import com.schoolproject.traveltour.model.Menu;
 import com.schoolproject.traveltour.utils.DataSet;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,7 +24,12 @@ public class MainActivity extends AppCompatActivity {
 
     private void initUI() {
         rv = findViewById(R.id.main_rv);
-        menuAdapter = new MenuAdapter(this, DataSet.getMenuList());
+        menuAdapter = new MenuAdapter(this, DataSet.getMenuList(), new MenuAdapter.MenuClickListener() {
+            @Override
+            public void onClick(Menu menu) {
+                Toast.makeText(MainActivity.this, menu.getTitle(), Toast.LENGTH_SHORT).show();
+            }
+        });
         rv.setAdapter(menuAdapter);
     }
 }
--- app/src/main/java/com/schoolproject/traveltour/adapter/MenuAdapter.java
+++ app/src/main/java/com/schoolproject/traveltour/adapter/MenuAdapter.java
@@ -18,10 +18,12 @@ import java.util.List;
 public class MenuAdapter extends RecyclerView.Adapter<MenuAdapter.ViewHolder> {
     private Context mContext;
     private List<Menu> dataSet;
+    private MenuClickListener menuClickListener;
 
-    public MenuAdapter(Context mContext, List<Menu> dataSet) {
+    public MenuAdapter(Context mContext, List<Menu> dataSet, MenuClickListener menuClickListener) {
         this.mContext = mContext;
         this.dataSet = dataSet;
+        this.menuClickListener = menuClickListener;
     }
 
     @NonNull
@@ -51,7 +53,7 @@ public class MenuAdapter extends RecyclerView.Adapter<MenuAdapter.ViewHolder> {
         return dataSet == null ? 0 : dataSet.size();
     }
 
-    static class ViewHolder extends RecyclerView.ViewHolder {
+    class ViewHolder extends RecyclerView.ViewHolder {
         private View view;
         private TextView title;
         private TextView description;
@@ -62,6 +64,17 @@ public class MenuAdapter extends RecyclerView.Adapter<MenuAdapter.ViewHolder> {
             view = itemView.findViewById(R.id.background);
             title = itemView.findViewById(R.id.title);
             description = itemView.findViewById(R.id.description);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    menuClickListener.onClick(dataSet.get(getAdapterPosition()));
+                }
+            });
         }
+    }
+
+    public interface MenuClickListener {
+        void onClick(Menu menu);
     }
 }
