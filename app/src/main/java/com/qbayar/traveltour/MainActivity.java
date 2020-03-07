a9f631d53e17b4c2c5ca11df2fb7eb61fc9505f4
NaingMyoThawNMYT <naingmyothaw.nmyt@gmial>
2020-03-07 13:07:32 +0630

    first commit

---
+++ app/src/main/java/com/qbayar/traveltour/MainActivity.java
@@ -0,0 +1,14 @@
+package com.qbayar.traveltour;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
