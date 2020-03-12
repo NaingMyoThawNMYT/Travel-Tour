4bac8d645c16e8bf6c030bf51c7a25cc7992d844
NaingMyoThawNMYT <naingmyothaw.nmyt@gmial>
2020-03-12 21:22:41 +0630

    - Added NewTourActivity.

--- app/src/main/java/com/schoolproject/traveltour/activity/MainActivity.java
+++ app/src/main/java/com/schoolproject/traveltour/activity/MainActivity.java
@@ -3,11 +3,13 @@ package com.schoolproject.traveltour.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.schoolproject.traveltour.R;
 import com.schoolproject.traveltour.adapter.MenuAdapter;
 import com.schoolproject.traveltour.model.Menu;
@@ -16,6 +18,7 @@ import com.schoolproject.traveltour.utils.DataSet;
 public class MainActivity extends AppCompatActivity {
     private RecyclerView rv;
     private MenuAdapter menuAdapter;
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +48,15 @@ public class MainActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setTitle("Popular Trip");
         }
+
+        fab = findViewById(R.id.fab);
+        fab.setVisibility(View.VISIBLE);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, NewTourActivity.class));
+            }
+        });
 
         rv = findViewById(R.id.main_rv);
         menuAdapter = new MenuAdapter(this, DataSet.getMenuList(), new MenuAdapter.MenuClickListener() {
---
+++ app/src/main/java/com/schoolproject/traveltour/activity/NewTourActivity.java
@@ -0,0 +1,16 @@
+package com.schoolproject.traveltour.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.schoolproject.traveltour.R;
+
+public class NewTourActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_tour);
+    }
+}
