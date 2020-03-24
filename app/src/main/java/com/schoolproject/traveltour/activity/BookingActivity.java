b7f774740ffd0b653a93c1d70514795ca5f63c9f
Naing Myo Thaw <naingmyothaw.nmyt@gmail.com>
2020-03-24 22:10:00 +0630

    - Add some top padding in Booking activity. - Show current user email in Booking activity.

--- app/src/main/java/com/schoolproject/traveltour/activity/BookingActivity.java
+++ app/src/main/java/com/schoolproject/traveltour/activity/BookingActivity.java
@@ -5,12 +5,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.schoolproject.traveltour.R;
 
 public class BookingActivity extends BaseSecondActivity {
     public static final String PACKAGE_TOUR = "package_tour";
     public static final String OPTIONAL_TOUR = "optional_tour";
     public static final String SIGHTSEEING_TOUR = "sightseeing_tour";
+
+    private FirebaseAuth mAuth;
+
+    private TextInputEditText edtName, edtPassportNo, edtPhone, edtEmail, edtAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +44,19 @@ public class BookingActivity extends BaseSecondActivity {
             // TODO: 10-Mar-20 book for sightseeing tour
         }
 
+        initUI();
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if (currentUser == null) {
+            Toast.makeText(this, "Please sign in first", Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+
+        edtEmail.setText(currentUser.getEmail());
+
         // TODO: 11-Mar-20 this is simple and will be delete in later
         findViewById(R.id.btn_book_now).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,5 +64,13 @@ public class BookingActivity extends BaseSecondActivity {
                 startActivity(new Intent(BookingActivity.this, InvoiceActivity.class));
             }
         });
+    }
+
+    private void initUI() {
+        edtName = findViewById(R.id.edt_name);
+        edtPassportNo = findViewById(R.id.edt_nrc);
+        edtPhone = findViewById(R.id.edt_phone);
+        edtEmail = findViewById(R.id.edt_email);
+        edtAddress = findViewById(R.id.edt_address);
     }
 }
