6c0d44a0a4d1500e1c04b1fc576ae042e10ed5c5
NaingMyoThawNMYT <naingmyothaw.nmyt@gmail.com>
2020-04-08 20:03:42 +0630

    - Sample google map activity is upgraded.

--- app/src/main/java/com/schoolproject/traveltour/activity/MapsActivity.java
+++ app/src/main/java/com/schoolproject/traveltour/activity/MapsActivity.java
@@ -3,6 +3,7 @@ package com.schoolproject.traveltour.activity;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,36 +14,42 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.schoolproject.traveltour.R;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    public static final String PARAM_LAT = "param_lat";
+    public static final String PARAM_LNG = "param_lng";
+    public static final String PARAM_TITLE = "param_title";
 
-    private GoogleMap mMap;
+    private double lat, lng;
+    private String title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        Bundle b = getIntent().getExtras();
+        if (b == null || !b.containsKey(PARAM_LAT) || !b.containsKey(PARAM_LNG)) {
+            Toast.makeText(this, "Invalid Location", Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+
+        lat = b.getDouble(PARAM_LAT);
+        lng = b.getDouble(PARAM_LNG);
+        title = b.getString(PARAM_TITLE);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add a marker in location and move the camera with zoom in animation
+        LatLng sydney = new LatLng(lat, lng);
+        googleMap.addMarker(new MarkerOptions().position(sydney).title(title));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney, 15.0f));
     }
 }
